5c6387dd20f084b010da9076fa3c29a1a2418d70
DumbTempest <rajattempest8736@gmail.com>
2024-11-13 16:06:34 +0530

    Changing stuff

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express();
+const port = 3001;
+import { tracking } from './tracking.js'
+
+app.set("view engine", "ejs");
+app.use(express.static("public")); // Serve static files
+
+// Route to render the tracked products page
+app.get("/", async (req, res) => {
+  try {
+    const products = await tracking.findAll();
+    res.render("index.ejs", { products });
+  } catch (error) {
+    res.status(500).send("Error fetching products");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ tracking.js
@@ -0,0 +1,119 @@
+// Import statements
+import cron from "node-cron";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import CC from "currency-converter-lt";
+import Sequelize from "sequelize";
+
+let CurrencyConverter = new CC();
+
+// Database configuration
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite", // SQLite only
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("| ✅ Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database: ", error);
+}
+
+// Define the 'tracking' model
+const tracking = sequelize.define("tracking", {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  price_INR: Sequelize.INTEGER,
+  price_USD: Sequelize.INTEGER,
+  product_image: Sequelize.STRING,
+  product_url: Sequelize.STRING,
+});
+
+await tracking.sync();
+console.log("| ✅ Table synced");
+
+// URLs to track
+const urls = [
+  "https://www.amazon.in/gp/product/B0BB9ZH2LK/ref=ox_sc_act_image_1?smid=A3LMMNCB8LRXW0&psc=1",
+  "https://www.amazon.in/ASUS-Battery-i7-12700H-Windows-FX507ZV-LP094W/dp/B0C4TW7328/?_encoding=UTF8&pd_rd_w=jHTm2&content-id=amzn1.sym.4d5b78c6-4f80-4b93-8d16-deb7aaa19e3f%3Aamzn1.symc.afd86303-4a72-4e34-8f6b-19828329e602&pf_rd_p=4d5b78c6-4f80-4b93-8d16-deb7aaa19e3f&pf_rd_r=94PTD8JAEMFMC55RTNN1&pd_rd_wg=OYGwn&pd_rd_r=38df7e9d-3514-42f2-be62-4bd4defedddb&ref_=pd_gw_ci_mcx_mr_hp_atf_m",
+  "https://www.amazon.in/gp/product/B09BVCVTBC/ref=ox_sc_saved_image_1?smid=A14CZOWI0VEHLG&psc=1",
+  "https://www.amazon.in/ASUS-Advantage-Battery-Sandstorm-FA617XS-N3026WS/dp/B0C4TW2SZM/ref=sr_1_1_sspa?crid=1FH1WBJ0VSBGZ&keywords=asus+tuf+f15&qid=1708280327&sprefix=asus+t%2Caps%2C256&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1",
+  "https://www.amazon.in/permanent-guarantee-Arduino-Uno/dp/B0044X2E5S/ref=sr_1_4?crid=1PMS364D3GP97&dib=eyJ2IjoiMSJ9.gGEp_d-Pdy1_dVQLfnjAocEVs_z0vRhvs57A-ntOQ_XiSXw6TYx24K0WtWSqveXSpyVpwTxH8NlBY0WxZMimw2shA4LMYSxQfDyl45tPQFBHTw00eynSNLsY222ZSz8j5rLeWYu_87KX8lE9fcpYA_eyftYcM9Z2IBinmz5yJI-p2XMh9rNCzUzVpV6APAMQRTEt6gcVnfwf_h6Gl2QwT9AE4MYu0x2ipry19Ab5su5nuP9p3sRSiggUQLfvJ0jUehxwZCFtNdzqlWOPaLecwKNW0QBYNXKOH0TBr3Ekopg.IpMwRvw1GAN0m9Qh-oKd6szbd0Oeb4Gx_-kB7t9v9bo&dib_tag=se&keywords=arduino+uno&nsdOptOutParam=true&qid=1731339158&sprefix=arduino%2Caps%2C326&sr=8-4",
+  "https://www.amazon.in/Razer-DeathAdder-Essential-White-RZ01-03850200-R3M1/dp/B092R71V77/ref=pd_bxgy_thbs_d_sccl_1/260-0398836-7123562?pd_rd_w=tEZLB&content-id=amzn1.sym.c7ef6266-a9b9-4df2-ad24-f795ecdadcdc&pf_rd_p=c7ef6266-a9b9-4df2-ad24-f795ecdadcdc&pf_rd_r=531H6D877DXAKK4NZKD9&pd_rd_wg=s7Mnl&pd_rd_r=ab620c2a-cd80-49e8-8b08-2b9d439faded&pd_rd_i=B092R71V77&psc=1",
+];
+
+// Define the scraping function
+async function scrapeWebsite(url) {
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const price_new = parseInt(
+      $(".a-price-whole").first().text().replace(/,/g, "")
+    );
+    const price_new_USD = await CurrencyConverter.from("INR")
+      .to("USD")
+      .amount(price_new)
+      .convert();
+    const title = $(".product-title-word-break")
+      .first()
+      .text()
+      .split(",")[0]
+      .trim();
+    const product_image = $("#imgTagWrapperId").first().find("img").attr("src");
+
+    const tracked = await tracking.findOne({ where: { name: title } });
+
+    if (tracked) {
+      const trackedWithSamePrice = await tracking.findAll({
+        where: { name: title, price_INR: price_new, price_USD: price_new_USD },
+      });
+      if (trackedWithSamePrice.length > 0)
+        return console.log(
+          `| ✅ Price for ${title} stayed same since last checked, No changes made`
+        );
+      await tracking.update(
+        { price_INR: price_new, price_USD: price_new_USD },
+        { where: { name: title } }
+      );
+      console.log(
+        `| ✅ The price of ${title} has Updated to ${price_new.toLocaleString()} INR and ${price_new_USD.toLocaleString()} USD`
+      );
+    } else {
+      await tracking.create({
+        name: title,
+        price_INR: price_new,
+        price_USD: price_new_USD,
+        product_image,
+        product_url: url,
+      });
+      console.log(`| ✅ Data for ${title} added to database`);
+    }
+  } catch (error) {
+    console.error("| ❌ Error fetching website:", error.name);
+  }
+}
+
+// Run scraping on schedule
+async function scrapeWebsites() {
+  const d = new Date();
+  console.log(`====================== ${d} ======================`);
+  for (const url of urls) {
+    await scrapeWebsite(url);
+  }
+}
+
+cron.schedule("0 */2 * * *", async () => {
+  await scrapeWebsites();
+});
+
+await scrapeWebsites();
+
+// Export tracking for use in other files
+export { tracking };
