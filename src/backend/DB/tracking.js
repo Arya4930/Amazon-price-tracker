5f55490bade691cbe2855b444a698414f2eef695
Arya <85472696+Arya4930@users.noreply.github.com>
2024-11-16 23:55:37 +0530

    Making more databases

---
+++ src/backend/DB/tracking.js
@@ -0,0 +1,18 @@
+import sequelize from "./dbconfig.js";
+import Sequelize from 'sequelize'
+
+const tracking = sequelize.define("tracking", {
+    name: {
+        type: Sequelize.STRING,
+        unique: true,
+    },
+    price_INR: Sequelize.INTEGER,
+    price_USD: Sequelize.INTEGER,
+    product_image: Sequelize.STRING,
+    product_url: Sequelize.STRING,
+});
+
+await tracking.sync();
+console.log("| ✅ Table Tracking synced");
+
+export default tracking
